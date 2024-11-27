de7a7fb5aefa06ef30d3e83701230ace0495ae1a
yash000017 <yashmodiofficial@gmail.com>
2024-11-27 14:37:31 +0530

    husky test

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button } from '@mantine/core';
-import './button.css'; // Import the CSS file for styling
+import './button.css';
 
 interface CustomButtonProps {
   label: string;
